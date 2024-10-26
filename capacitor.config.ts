dc0a4abc9e27b1ee9a491358d2e435e047a55b2d
Jotaig <joseignaciojpj@gmail.com>
2024-10-26 11:39:41 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'JosePuga198482595',
+  webDir: 'www'
+};
+
+export default config;
